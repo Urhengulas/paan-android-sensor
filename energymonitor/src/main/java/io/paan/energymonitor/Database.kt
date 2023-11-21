3cfa199d7268b3902285c2a9e902e199b4268c77
Johann Hemmann <johann.hemmann@code.berlin>
2023-11-21 13:25:35 +0100

    Fix name

--- energymonitor/src/main/java/io/paan/energymonitor/Database.kt
+++ energymonitor/src/main/java/io/paan/energymonitor/Database.kt
@@ -5,15 +5,14 @@ import io.realm.kotlin.Realm
 import io.realm.kotlin.mongodb.App
 import io.realm.kotlin.mongodb.AppConfiguration
 import io.realm.kotlin.mongodb.Credentials
-import io.realm.kotlin.mongodb.User
 import io.realm.kotlin.mongodb.annotations.ExperimentalAsymmetricSyncApi
 import io.realm.kotlin.mongodb.sync.SyncConfiguration
 import kotlinx.coroutines.runBlocking
 import io.realm.kotlin.mongodb.ext.insert as insert2
 
 class Database(appId: String, token: String) {
-    private val app2: App = App.create(AppConfiguration.Builder(appId).build())
-    private val user = runBlocking { app2.login(Credentials.apiKey(token)) }
+    private val app: App = App.create(AppConfiguration.Builder(appId).build())
+    private val user = runBlocking { app.login(Credentials.apiKey(token)) }
     private val config: SyncConfiguration = SyncConfiguration.create(user, setOf(Energy::class))
     private val realm: Realm = Realm.open(config)
 
